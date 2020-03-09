b110570757107fbf74d6d411da337032f9a66534
starosta-coder <54850884+starosta-coder@users.noreply.github.com>
2020-03-09 17:52:51 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a13 = 4, b13 = 2, res13 = 0;
+            res13 = Convert.ToInt32(Math.Pow((a13 - b13), 2));
+            Console.WriteLine(res13);
+        }
+    }
+}
